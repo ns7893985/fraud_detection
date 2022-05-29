515ac3b62698bdfa999a1b3077cd6645a9162129
ns7893985 <ns7893985@gmail.com>
2022-05-29 23:31:09 +0530

    added 3 new ML Model code

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("adding all ML related function")
+
+def model1():
+    prin("adding code for ML model1")
+
+def model2():
+    prin("adding code for ML model2")
+
+def model3():
+    prin("adding code for ML model3")
